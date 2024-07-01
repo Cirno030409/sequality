43745ebd1a79bd0c1db370457a3c5f0c2f1c53ea
Cirno030409 <taniymail@gmail.com>
2024-07-01 10:03:59 +0900

    fixed calulatetestjava

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class CalculateTest {
   @Test
   public void testSum() {
-    Calculate calculate = new Calculate()
+    Calculate calculate = new Calculate();
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
