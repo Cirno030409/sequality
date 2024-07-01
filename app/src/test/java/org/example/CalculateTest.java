494387f688a9304c2686711eb14f8368cdeea459
Cirno030409 <taniymail@gmail.com>
2024-07-01 09:59:48 +0900

    add testappreturnhelloworld

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.*;
 
 public class CalculateTest {
   @Test
-  public void testAppReternHelloWorld() {
-    App classUnderTest = new App();
-    assertEquals("Hello World!", classUnderTest.getGreeting());
+  public void testSum() {
+    Calculate calculate = new Calculate()
+    int expected = 5;
+    assertEquals(expected, calculate.sum(2, 3));
   }
 }
